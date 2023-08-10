f4d5cc6be26c1cdffe697ce445a3bbf021bb56f3
Documenter.jl <documenter@juliadocs.github.io>
2023-08-10 08:20:16 +0000

    build based on 7103879

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"refs/api/#API","page":"API","title":"API","text":"","category":"section"},{"location":"#NUMA.jl","page":"NUMA","title":"NUMA.jl","text":"","category":"section"},{"location":"","page":"NUMA","title":"NUMA","text":"TBD","category":"page"}]
+}
